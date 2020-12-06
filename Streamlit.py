d6702e470a81354a8c366a65ae9a88f692cd3a10
Chiehcode <56834325+Chiehcode@users.noreply.github.com>
2020-12-06 21:09:48 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from pycaret.classification import *
+# from pycaret.classification import *
 import base64
 import io
 import requests
